5b26fb1ada8195c831686d4d6320d9a1e36a5390
kaushik nandhan injamuri <kaushik nandhan injamuri>
2017-02-21 15:45:26 -0800

    feat(SpringMvcRest): add swgger for documentation

--- SpringMvcRest/src/main/java/io/springmvcrest/WebAppConfiguration.java
+++ SpringMvcRest/src/main/java/io/springmvcrest/WebAppConfiguration.java
@@ -3,10 +3,22 @@ package io.springmvcrest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
-public class WebAppConfiguration {
+public class WebAppConfiguration extends WebMvcConfigurerAdapter{
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+	       registry.addResourceHandler("swagger-ui.html")
+	                .addResourceLocations("classpath:/META-INF/resources/");
+
+	        registry.addResourceHandler("/webjars/**")
+	                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+	}
 
 }
